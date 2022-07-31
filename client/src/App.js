9908b7024f3b0fee11994989d92bd1aba57fdbb4
Blake Videbeck <blake.videbeck@ezyvet.com>
2022-07-31 14:41:40 +1200

    Set up projects and project card components and get project mutations

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { ApolloProvider, ApolloClient } from '@apollo/client';
 import { Clients } from './components/Clients';
 import { cache } from './utils/cache';
 import { AddClientModal } from './components/AddClientModal';
+import { Projects } from './components/Projects';
 
 const client = new ApolloClient({
 	uri: 'http://localhost:8000/graphql',
@@ -15,6 +16,7 @@ const App = () => {
 			<Header />
 			<div className='container'>
 				<AddClientModal />
+				<Projects />
 				<Clients />
 			</div>
 		</ApolloProvider>
