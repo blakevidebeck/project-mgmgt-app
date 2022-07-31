e43346e6b40ffb2815547a8de45c1f59da6ea56d
Blake Videbeck <blake.videbeck@ezyvet.com>
2022-07-31 14:05:53 +1200

    Add client mutation and modal

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import Header from './components/Header';
 import { ApolloProvider, ApolloClient } from '@apollo/client';
 import { Clients } from './components/Clients';
 import { cache } from './utils/cache';
+import { AddClientModal } from './components/AddClientModal';
 
 const client = new ApolloClient({
 	uri: 'http://localhost:8000/graphql',
@@ -13,6 +14,7 @@ const App = () => {
 		<ApolloProvider client={client}>
 			<Header />
 			<div className='container'>
+				<AddClientModal />
 				<Clients />
 			</div>
 		</ApolloProvider>
